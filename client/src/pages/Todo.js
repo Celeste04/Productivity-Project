51fba88f63fd81a4dad6f5785aa162f7ccc12615
j75kim <125719766+j75kim@users.noreply.github.com>
2023-09-07 10:14:20 -0400

    Added routes

---
+++ client/src/pages/Todo.js
@@ -0,0 +1,11 @@
+import React, {} from "react";
+
+export default function Todo() {
+    return (
+        <div className="page">
+            <h1 className="headline">
+                To-Do List
+            </h1>
+        </div>
+    );
+};
