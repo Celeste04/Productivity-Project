5838cdda980556b4e2c7c27bae733792ca11aa26
Serena X <84420380+xuserena12@users.noreply.github.com>
2023-09-07 23:51:48 -0400

    Work on pomodoro page

--- client/src/pages/Timer.js
+++ client/src/pages/Timer.js
@@ -1,5 +1,16 @@
 export default function Timer () {
     return (
-        <h1> Timer</h1>
+        <div class="bg-green h-screen w-screen">
+            <h1 class="text-4xl font-bold text-center text-grey p-10">Pomodoro</h1>
+            <div class="text-3xl font-bold text-grey ml-20 p-10">🌱 focus zone</div>
+            <div class="relative p-100">
+                <div class="absolute left-20">
+                    left
+                </div>
+                <div class="absolute right-20">
+                    right
+                </div>
+            </div>
+        </div>
     );
 };
