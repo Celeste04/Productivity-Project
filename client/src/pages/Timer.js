79ca2c4b3d93ae00328dbb7b06fef44df7a32e09
Serena X <84420380+xuserena12@users.noreply.github.com>
2023-09-10 16:55:05 -0400

    pomodoro styling

--- client/src/pages/Timer.js
+++ client/src/pages/Timer.js
@@ -1,16 +1,23 @@
 export default function Timer () {
     return (
-        <div class="bg-green h-screen w-screen">
-            <h1 class="text-4xl font-bold text-center text-grey p-10">Pomodoro</h1>
-            <div class="text-3xl font-bold text-grey ml-20 p-10">🌱 focus zone</div>
-            <div class="relative p-100">
-                <div class="absolute left-20">
-                    left
+        <div className="bg-green h-screen w-screen">
+          <h1 className="text-4xl font-bold text-center text-grey p-10">Pomodoro</h1>
+          <h2 className="text-3xl font-bold text-grey ml-20 p-10">🌱 focus zone</h2>
+          <div className="flex space-x-5 p-10">
+            <div className="bg-light-pink rounded-xl w-1/2 p-5 border border-pink">
+              <div className="w-full h-100 flex justify-center bg-pink rounded-xl">
+                <div>
+                    <div className="p-8"></div>
+                    <div></div>
                 </div>
-                <div class="absolute right-20">
-                    right
-                </div>
+              </div>
             </div>
+            <div className="bg-light-pink rounded-xl w-1/2 p-5 border border-pink">
+              <div className="w-full h-100 flex justify-center items-center bg-pink rounded-xl">
+                <img className="rounded-xl w-5/6" src="/images/Lofi_Girl.jpeg"/>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,11 +5,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      height: {
+        '100' : '28rem',
+
+      },
       colors: {
         'green': '#D4E2D4',
         'cream': '#FAF3F0',
         'blue': '#CBE9F0',
         'pink':'#F8DADB',
+        'light-pink':'#FAF3F0',
         'grey': '#585858',
         'light-grey': "#D9D9D9",
       },
